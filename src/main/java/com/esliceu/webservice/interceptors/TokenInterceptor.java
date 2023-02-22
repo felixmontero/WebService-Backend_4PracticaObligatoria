71431aeef6ceb4ffff4207608e30e074546b51fe
felix <fmontero798@gmail.com>
2023-02-22 17:23:29 +0100

    Interceptor validate token

---
+++ src/main/java/com/esliceu/webservice/interceptors/TokenInterceptor.java
@@ -0,0 +1,34 @@
+package com.esliceu.webservice.interceptors;
+import com.esliceu.webservice.services.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.HandlerInterceptor;
+@Component
+public class TokenInterceptor implements HandlerInterceptor {
+    @Autowired
+    TokenService tokenService;
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(request.getMethod().equals("OPTIONS")){
+            return true;
+        }
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Bad credentials");
+        }
+        try {
+            String token = authHeader = authHeader.replace("Bearer ","");
+            String userName = tokenService.getUser(token);
+            request.setAttribute("user",userName);
+            return true;
+        }catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Bad credentials or expired token");
+        }
+
+    }
+}
